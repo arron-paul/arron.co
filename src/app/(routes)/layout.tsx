4a7dea22a42d62a004dda32a601d4ca7a2ed1c09
Arron Paul <hello@arron.io>
2024-02-17 14:17:04 +0000

    Scaffolding for navigation routes

---
+++ src/app/(routes)/layout.tsx
@@ -0,0 +1,15 @@
+import Header from '@/components/Header'
+import '@/app/styles/globals.css'
+
+export default function RoutesPageLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  )
+}
