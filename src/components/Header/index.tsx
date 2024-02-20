77a5bdcf0b156b2e37c6dca209c76c1237cfc906
Arron Paul <hello@arron.io>
2024-02-20 08:37:38 +0000

    Branding

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,28 @@
 import Navigation from '@/components/Navigation'
+import Image from 'next/image'
 
 export default function Header() {
   return (
     <header className="py-4">
       <div className="max-w-5xl mx-auto px-4 flex justify-between items-center">
-        <svg width="24" height="24">
-          <circle cx="12" cy="12" r="12" fill="red" />
-        </svg>
+        <div className="flex">
+          <Image
+            src="/poly.svg"
+            alt="Logo"
+            className="dark:invert"
+            width={24}
+            height={24}
+            priority
+          />
+          <Image
+            src="/wordmark.svg"
+            alt="Logo"
+            className="pl-4 dark:invert"
+            width={100}
+            height={24}
+            priority
+          />
+        </div>
         <Navigation />
       </div>
     </header>
