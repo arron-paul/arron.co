31f9d3446e8af02757015b5139e28f490eb3e3aa
Arron Paul <hello@arron.io>
2024-02-21 18:29:27 +0000

    Added barrel for components

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { usePathname } from 'next/navigation'
-import NavigationItem from '@/components/navigation-item'
+import { NavigationItem } from '@/components'
 
 const links = [
   {
