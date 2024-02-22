38fff539152c2c6c71485e6b45f3db90bef28b91
Arron Paul <hello@arron.io>
2024-02-22 10:18:13 +0000

    Removing MDX for now

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,4 @@
-import nextMdx from '@next/mdx'
-import remarkFrontmatter from 'remark-frontmatter'
-
-const withMdx = nextMdx({
-  options: {
-    remarkPlugins: [remarkFrontmatter],
-    experimental: {
-      mdxRs: true,
-    },
-  },
-})
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-  experimental: {
-    mdxRs: false,
-  },
-}
+const nextConfig = {}
 
-export default withMdx(nextConfig)
+export default nextConfig
