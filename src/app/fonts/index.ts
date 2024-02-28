066a2677bdcc92a271fbbc069de47bbc23aaee7b
Arron Paul <hello@arron.io>
2024-02-28 13:28:53 +0000

    Using font

---
+++ src/app/fonts/index.ts
@@ -0,0 +1,98 @@
+import localFont from 'next/font/local'
+
+export const cerebri = localFont({
+  src: [
+    {
+      path: './cerebri-sans-pro/CerebriSansPro-Bold.woff',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './cerebri-sans-pro/CerebriSansPro-BoldItalic.woff',
+      weight: '700',
+      style: 'italic',
+    },
+    {
+      path: './cerebri-sans-pro/CerebriSansPro-ExtraBold.woff',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: './cerebri-sans-pro/CerebriSansPro-ExtraBoldItalic.woff',
+      weight: '800',
+      style: 'italic',
+    },
+    {
+      path: './cerebri-sans-pro/CerebriSansPro-ExtraLight.woff',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: './cerebri-sans-pro/CerebriSansPro-ExtraLightItalic.woff',
+      weight: '100',
+      style: 'italic',
+    },
+    {
+      path: './cerebri-sans-pro/CerebriSansPro-Heavy.woff',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: './cerebri-sans-pro/CerebriSansPro-HeavyItalic.woff',
+      weight: '900',
+      style: 'italic',
+    },
+    {
+      path: './cerebri-sans-pro/CerebriSansPro-Light.woff',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: './cerebri-sans-pro/CerebriSansPro-LightItalic.woff',
+      weight: '200',
+      style: 'italic',
+    },
+    {
+      path: './cerebri-sans-pro/CerebriSansPro-Medium.woff',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: './cerebri-sans-pro/CerebriSansPro-MediumItalic.woff',
+      weight: '300',
+      style: 'italic',
+    },
+    {
+      path: './cerebri-sans-pro/CerebriSansPro-Regular.woff',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './cerebri-sans-pro/CerebriSansPro-RegularItalic.woff',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: './cerebri-sans-pro/CerebriSansPro-SemiBold.woff',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './cerebri-sans-pro/CerebriSansPro-SemiBoldItalic.woff',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: './cerebri-sans-pro/CerebriSansPro-Thin.woff',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: './cerebri-sans-pro/CerebriSansPro-ThinItalic.woff',
+      weight: '200',
+      style: 'italic',
+    },
+  ],
+  variable: '--font-sans',
+  display: 'auto',
+})
