df6ededfea86bd5eda270cec037b7e2c95f5894c
Arron Paul <hello@arron.io>
2024-02-28 17:08:43 +0000

    Added about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import { allPosts } from '@/.contentlayer/generated'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div className="prose dark:prose-invert">
+      <article>
+        <p>Hello World</p>
+      </article>
+    </div>
+  )
+}
