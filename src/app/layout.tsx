68aa7d39d0ed5a823a0b12cd1c2fb2659810ce56
Arron Paul <hello@arron.io>
2024-07-17 21:18:08 +0100

    The fancy iOS cursor needs to go. Accessibility nightmare.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
     <html lang="en">
       <body
         className={`antialiased min-h-screen bg-white dark:bg-slate-950 text-slate-900 dark:text-slate-50 ${cerebri.className}`}>
-        <FancyCursor />
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <div className="max-w-2xl mx-auto py-10 px-4">
             <header className="flex items-center justify-between">
